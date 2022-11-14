76c51f9ab3a32df87ad2bc53f346de605ce8a28c
Rybakov-Ilay <ilay106@mail.ru>
2022-11-14 22:32:30 +0300

    fix: hover state for all links and buttons

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 export default function Auth(props) {
   return (
     <section className="auth">
-      <img className="auth__logo" src={logo} alt="Логотип"/>
+      <Link className="auth__logo" to="/">
+        <img src={logo} alt="Логотип"/>
+      </Link>
       <h2 className="auth__title">{props.title}</h2>
       <form className="auth__form">
         {props.children}
